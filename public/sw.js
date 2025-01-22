16a145d46ebfc419f04cd1fab94ca43dfc738e8d
Facundo García Martoni <43690718+facmartoni@users.noreply.github.com>
2025-01-22 18:18:31 -0300

    Ended up not using workbox-cli. Used workbox-recipes instead.

---
+++ public/sw.js
@@ -0,0 +1,15 @@
+importScripts(
+    'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+);
+
+const {pageCache, imageCache, staticResourceCache, offlineFallback, googleFontsCache} = workbox.recipes;
+const {setDefaultHandler} = workbox.routing;
+const {NetworkOnly} = workbox.strategies;
+
+setDefaultHandler(new NetworkOnly());
+
+// pageCache(); // Use this if I will cache the Task Create page.
+googleFontsCache();
+staticResourceCache();
+imageCache();
+offlineFallback();
